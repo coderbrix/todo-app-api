a68e19b238032bdcff947ff7475a002545395d40
kh-anik26 <kamrulhassananik8@gmail.com>
2026-04-07 13:50:21 +0600

    todo changes

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -9,41 +9,41 @@ export class TodoController {
     this.todoService = new TodoService();
   }
   async createTodo(req: Request, res: Response) {
-    const userId = getUserIdOrThrow(req);
+    const userId = getUserIdOrThrow(req).toString();
     const todo = await this.todoService.createTodo(req.body, userId);
     res.status(201).json(todo);
   }
 
   async getWorkspaceTodos(req: Request, res: Response) {
-    const userId = getUserIdOrThrow(req);
+    const userId = getUserIdOrThrow(req).toString();
     const { workspaceId } = req.params;
     const todos = await this.todoService.getWorkspaceTodos(workspaceId as string, userId, req.query);
     res.json(todos);
   }
 
   async getTodoById(req: Request, res: Response) {
-    const userId = getUserIdOrThrow(req);
+    const userId = getUserIdOrThrow(req).toString();
     const { id } = req.params;
     const todo = await this.todoService.getTodoById(id as string, userId);
     res.json(todo);
   }
 
   async updateTodo(req: Request, res: Response) {
-    const userId = getUserIdOrThrow(req);
+    const userId = getUserIdOrThrow(req).toString();
     const { id } = req.params;
     const todo = await this.todoService.updateTodo(id as string, req.body, userId);
     res.json(todo);
   }
 
   async deleteTodo(req: Request, res: Response) {
-    const userId = getUserIdOrThrow(req);
+    const userId = getUserIdOrThrow(req).toString();
     const { id } = req.params;
     await this.todoService.deleteTodo(id as string, userId);
     res.status(204).send();
   }
 
   async reorderTodo(req: Request, res: Response) {
-    const userId = getUserIdOrThrow(req);
+    const userId = getUserIdOrThrow(req).toString();
     const { id } = req.params;
     const { newPosition } = req.body;
 
